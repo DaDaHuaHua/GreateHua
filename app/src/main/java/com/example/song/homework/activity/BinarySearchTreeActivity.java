5a08a9a91c14e8c65293a9947b18f56480c7e650
songhua <13260656511@163.com>
2017-11-19 02:46:07 +0800

    【新特性】提交搜索二叉树删除操作，增加了额外两种方法

--- app/src/main/java/com/example/song/homework/activity/BinarySearchTreeActivity.java
+++ app/src/main/java/com/example/song/homework/activity/BinarySearchTreeActivity.java
@@ -10,10 +10,19 @@ import com.example.song.homework.bean.BinarySearchTree;
  * <p> Created by 宋华 on 2017/11/16.
  */
 public class BinarySearchTreeActivity extends BaseActivity {
+    BinarySearchTree<Integer> data = new BinarySearchTree<>();
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        BinarySearchTree<Integer> data = new BinarySearchTree<>();
+
+//         test01();
+//        testDel02();
+        testDel03();
+        data.printMidOrder();
+    }
+
+    private void test01() {
         data.add(10);
         data.add(2);
         data.add(3);
@@ -28,9 +37,61 @@ public class BinarySearchTreeActivity extends BaseActivity {
         data.add(150);
         data.add(115);
         data.add(14);
-        data.set(14,11);
-        data.delete(100);
-        data.delete(10);
-        data.printMidOrder();
+        data.set(14, 11);
+        data.delete01(100);
+        data.delete01(10);
+    }
+
+    private void testDel02() {
+        data.add(10);
+        data.add(2);
+        data.add(100);
+        data.add(1);
+        data.add(5);
+        data.add(4);
+        data.add(6);
+        data.add(80);
+        data.add(120);
+        data.add(70);
+        data.add(90);
+        data.add(110);
+        data.add(140);
+        data.add(105);
+        data.add(115);
+        data.add(106);
+        data.add(118);
+        data.add(117);
+        data.delete02(120);
+    }
+
+    private void testDel03() {
+        data.add(10);
+        data.add(2);
+        data.add(100);
+        data.add(1);
+        data.add(5);
+        data.add(4);
+        data.add(6);
+        data.add(80);
+        data.add(120);
+        data.add(70);
+        data.add(90);
+        data.add(110);
+        data.add(140);
+        data.add(105);
+        data.add(115);
+        data.add(106);
+        data.add(118);
+        data.add(117);
+        data.delete03(100);
+        data.delete03(120);
+        data.delete03(140);
+        data.delete03(105);
+        data.delete03(106);
+        data.delete03(110);
+        data.delete03(115);
+        data.delete03(117);
+        data.delete03(118);
+
     }
 }
--- app/src/main/java/com/example/song/homework/bean/BinarySearchTree.java
+++ app/src/main/java/com/example/song/homework/bean/BinarySearchTree.java
@@ -46,7 +46,12 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
     }
 
-    public void delete(T t) {
+    /**
+     * 方法一：
+     *
+     * @param t
+     */
+    public void delete01(T t) {
         TreeNode<T> node = get(t);
         if (node == null) {
             throw new NoSuchElementException("没有该元素");
@@ -100,22 +105,21 @@ public class BinarySearchTree<T extends Comparable<T>> {
         else {
             //右子节点 没有左子节点
             if (node.rightChild.leftChild == null) {
-                if (node.parent == null) {
+                if (parent == null) {
                     mRoot = node.rightChild;
-                    node.leftChild.parent = node.rightChild;
-                    node.rightChild.leftChild = node.leftChild;
                 } else {
                     if (parent.rightChild == node) {
                         parent.rightChild = node.rightChild;
                     } else {
                         parent.leftChild = node.rightChild;
                     }
-                    node.rightChild.leftChild = node.leftChild;
                     node.rightChild.parent = parent;
-                    node.leftChild.parent = node.rightChild;
                 }
+                node.leftChild.parent = node.rightChild;
+                node.rightChild.leftChild = node.leftChild;
             } else {
                 TreeNode<T> minNode = getMinNode(node.rightChild);
+
                 minNode.leftChild = node.leftChild;
                 node.leftChild.parent = minNode;
 
@@ -125,9 +129,9 @@ public class BinarySearchTree<T extends Comparable<T>> {
                 minNode.rightChild = node.rightChild;
                 node.rightChild.parent = minNode;
 
-                if(parent == null ){
+                if (parent == null) {
                     mRoot = minNode;
-                }else{
+                } else {
                     if (parent.rightChild == node) {
                         parent.rightChild = minNode;
                     } else {
@@ -142,6 +146,174 @@ public class BinarySearchTree<T extends Comparable<T>> {
         node.parent = null;
     }
 
+    /**
+     * 方法二
+     * 当删除节点既有左子树也有右子树时，先转移是数据，再删除
+     */
+    public void delete02(T t) {
+        TreeNode<T> node = get(t);
+        if (node == null) {
+            throw new NoSuchElementException("没有该元素");
+        }
+        TreeNode<T> parent = node.parent;
+
+        //是叶子节点
+        if (node.leftChild == null && node.rightChild == null) {
+            if (parent == null) {
+                mRoot = null;
+            } else {
+                //在parent的左边
+                if (parent.leftChild == node) {
+                    parent.leftChild = null;
+                } else {
+                    parent.rightChild = null;
+                }
+            }
+        }//只有左子节点
+        else if (node.leftChild != null && node.rightChild == null) {
+            //根节点
+            if (mRoot == null) {
+                mRoot = node.leftChild;
+                node.leftChild.parent = null;
+                node.leftChild = null;
+            } else {
+                //在parent的左边
+                if (parent.leftChild == node) {
+                    parent.leftChild = node.leftChild;
+                } else {
+                    parent.rightChild = node.leftChild;
+                }
+                node.leftChild.parent = parent;
+                node.leftChild = null;
+            }
+        }//只有右子节点 或 既有左子节点也有右子节点
+        else if (node.leftChild == null && node.rightChild != null) {
+            if (parent == null) {
+                mRoot = node.rightChild;
+                node.rightChild.parent = null;
+            } else {
+                //在parent左边
+                if (parent.leftChild == node) {
+                    parent.leftChild = node.rightChild;
+                } else {
+                    parent.rightChild = node.rightChild;
+                }
+                node.rightChild.parent = parent;
+            }
+        }//既有左子节点， 也有右子节点
+        else {
+            //右子节点 没有左子节点
+            if (node.rightChild.leftChild == null) {
+                if (parent == null) {
+                    mRoot = node.rightChild;
+                } else {
+                    if (parent.rightChild == node) {
+                        parent.rightChild = node.rightChild;
+                    } else {
+                        parent.leftChild = node.rightChild;
+                    }
+                    node.rightChild.parent = parent;
+                }
+                node.leftChild.parent = node.rightChild;
+                node.rightChild.leftChild = node.leftChild;
+            } else {
+                //1.找出右子树的最小节点
+                TreeNode<T> minMode = getMinNode(node.rightChild);
+                //2.将最小节点的值设置到删除的节点上
+                node.data = minMode.data;
+                //3.删除最小节点
+                TreeNode<T> minParent = minMode.parent;
+                //3.1 最小节点有右子树
+                if (minMode.rightChild != null) {
+                    minMode.rightChild.parent = minParent;
+                    minParent.leftChild = minMode.rightChild;
+                    minMode.rightChild = null;
+                } else {
+                    minParent.leftChild = null;
+                }
+                minMode.parent = null;
+            }
+        }
+    }
+
+    /**
+     * 方法三
+     * 优点：1 当删除的节点存在左右子节点时，不用考虑删除的是否为根节点
+     * 2.移动数据后再删除最大值，不存在节点的移动，效率更高
+     * 当删除节点既有左子树也有右子树时，先转移是数据，再删除
+     */
+    public void delete03(T t) {
+        TreeNode<T> node = get(t);
+        if (node == null) {
+            throw new NoSuchElementException("没有该元素");
+        }
+        TreeNode<T> parent = node.parent;
+
+        //是叶子节点
+        if (node.leftChild == null && node.rightChild == null) {
+            if (parent == null) {
+                mRoot = null;
+            } else {
+                //在parent的左边
+                if (parent.leftChild == node) {
+                    parent.leftChild = null;
+                } else {
+                    parent.rightChild = null;
+                }
+            }
+        }//只有左子节点
+        else if (node.leftChild != null && node.rightChild == null) {
+            //根节点
+            if (mRoot == null) {
+                mRoot = node.leftChild;
+                node.leftChild.parent = null;
+                node.leftChild = null;
+            } else {
+                //在parent的左边
+                if (parent.leftChild == node) {
+                    parent.leftChild = node.leftChild;
+                } else {
+                    parent.rightChild = node.leftChild;
+                }
+                node.leftChild.parent = parent;
+                node.leftChild = null;
+            }
+        }//只有右子节点
+        else if (node.leftChild == null && node.rightChild != null) {
+            if (parent == null) {
+                mRoot = node.rightChild;
+                node.rightChild.parent = null;
+            } else {
+                //在parent左边
+                if (parent.leftChild == node) {
+                    parent.leftChild = node.rightChild;
+                } else {
+                    parent.rightChild = node.rightChild;
+                }
+                node.rightChild.parent = parent;
+            }
+        }//既有左子节点， 也有右子节点
+        else {
+            //1.先找左子树的最大值
+            TreeNode<T> maxNode = getMaxNode(node.leftChild);
+            //2.将该最大值赋给节点
+            node.data = maxNode.data;
+            //3.删除最大值的节点
+            TreeNode<T> maxParent = maxNode.parent;
+            //3.1 有左节点
+            if (maxNode.leftChild != null) {
+                maxNode.leftChild.parent = maxParent;
+                maxParent.rightChild = maxNode.leftChild;
+                maxNode.leftChild = null;
+            } else {//3.2没有左节点（叶子节点）
+                maxParent.rightChild = null;
+            }
+            maxNode.parent = null;
+        }
+    }
+
+
+
     private TreeNode<T> getMinNode(TreeNode<T> start) {
         if (start == null) {
             return null;
@@ -149,6 +321,17 @@ public class BinarySearchTree<T extends Comparable<T>> {
         TreeNode<T> node = start;
         while (node.leftChild != null) {
             node = node.leftChild;
+        }
+        return node;
+    }
+
+    private TreeNode<T> getMaxNode(TreeNode<T> start) {
+        if (start == null) {
+            return null;
+        }
+        TreeNode<T> node = start;
+        while (node.rightChild != null) {
+            node = node.rightChild;
         }
         return node;
     }
